254195b33e380d44c66872a90511dab9a413b475
blablupcom <o.volodin@yahoo.com>
2016-04-03 21:42:50 +0400

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -91,11 +91,12 @@ url = "http://www.gateshead.gov.uk/Council and Democracy/finance/500Expenditure.
 errors = 0
 data = []
 header = {'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.9; rv:32.0) Gecko/20100101 Firefox/32.0',}
-proxy = {'http': 'http://195.89.201.48:80'}
+#proxy = {'http': 'http://195.89.201.48:80'}
 
 #### READ HTML 1.2
 
-html = requests.get(url, proxies = proxy, headers = header)   # using requests with proxy
+#html = requests.get(url, proxies = proxy, headers = header)   # using requests with proxy
+html = requests.get(url, headers = header) 
 soup = BeautifulSoup(html.text, 'lxml')
 
 #### SCRAPE DATA
